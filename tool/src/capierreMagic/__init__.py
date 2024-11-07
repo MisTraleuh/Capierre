2bbfb1bd63abac4cad485e991396cac0f38bfdf9
Nathan <nathan.catalan@epitech.eu>
2024-11-07 13:47:47 +0100

    [FIX] add \0 at the end of the MAGIC_NUMBER_END

--- tool/src/capierreMagic/__init__.py
+++ tool/src/capierreMagic/__init__.py
@@ -2,6 +2,6 @@
 class CapierreMagic():
     def __init__(self):
         self.MAGIC_NUMBER_START = b"CAPIERRE"
-        self.MAGIC_NUMBER_END = b"EERIPAC"
+        self.MAGIC_NUMBER_END = b"EERIPAC\0"
         self.MAGIC_NUMBER_START_LEN = len(self.MAGIC_NUMBER_START)
         self.MAGIC_NUMBER_END_LEN = len(self.MAGIC_NUMBER_END)
