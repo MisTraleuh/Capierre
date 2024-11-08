730dbd10a83cb3e8b09471e3c894fc74034efd16
WhiiteRose <tom.roger@epitech.eu>
2024-11-08 15:01:29 +0100

    [ADD] Qt Basic Design for UI application without style

---
+++ gui/main.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
+
+from sidebar_ui import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.sidebar_full.hide()
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
