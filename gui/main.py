64903c83bcea8c1303bdcae974986f85cb706f26
WhiiteRose <tom.roger@epitech.eu>
2024-11-22 18:35:44 +0100

    [ADD] Function for navigate through pages

--- gui/main.py
+++ gui/main.py
@@ -14,6 +14,25 @@ class MainWindow(QMainWindow):
         self.ui.sidebar_full.hide()
         self.ui.stackedWidget.setCurrentIndex(0)
 
+    def on_retrieve_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_retrieve_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_hide_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
+    def on_hide_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
+    def on_challenges_btn_1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def on_challenges_btn_2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+        
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
