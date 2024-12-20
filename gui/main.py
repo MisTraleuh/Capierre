fb352ea1591f763292a45b2305428843320b68e8
WhiiteRose <tom.roger@epitech.eu>
2024-12-20 13:24:36 +0100

    [ADD] style change and open file features

--- gui/main.py
+++ gui/main.py
@@ -1,8 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
-
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from sidebar_ui import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,7 +11,17 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.sidebar_full.hide()
+
         self.ui.stackedWidget.setCurrentIndex(0)
+
+        self.ui.open_button_2.clicked.connect(self.clicker)
+        self.ui.open_button.clicked.connect(self.clicker)
+
+
+    def clicker(self):
+        fname, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;Python Files (*.py)")
+        if fname:
+            self.ui.lineEdit_4.setText(fname)
 
     def on_retrieve_btn_1_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(0)
@@ -31,14 +40,18 @@ class MainWindow(QMainWindow):
 
     def on_challenges_btn_2_toggled(self):
         self.ui.stackedWidget.setCurrentIndex(2)
-        
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    with open('style.qss', 'r') as style_files:
-        style_str = style_files.read()
-    app.setStyleSheet(style_str)
+    try:
+        with open('style.qss', 'r') as style_files:
+            style_str = style_files.read()
+        app.setStyleSheet(style_str)
+    except FileNotFoundError:
+        print("Style file not found. Proceeding without stylesheet.")
+
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
