e0ff8741a806701ab64a8e1221733e655e66954a
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2025-03-03 14:10:54 +0100

    fix(capierreInterface): trailing line + disable pylint options

--- tool/src/capierreInterface/__init__.py
+++ tool/src/capierreInterface/__init__.py
@@ -1,10 +1,11 @@
-# pylint: disable=C0115
+# pylint: disable=C0115,C0114,C0103
 
 class Instruction:
     size: int
     address: int
     mnemonic: str
     op_str: str
+
 
 class Node:
     class Block:
